add15b55825015d6859b34c0765e5e72b1346509
karianmash <ianmachariak17@gmail.com>
2023-01-30 07:15:08 +0300

    [add] Added some styling to the console output

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
  * This is the entry point for the application
  */
 
-console.log("%c *Welcome to learning JavaScript with TDS!*", "color: aqua");
+console.log(
+  "%c *Welcome to learning JavaScript with Talanta Digital School!*",
+  "color: aqua; font-size: 14px; font-weight: bold;"
+);
