450f7718dd17c6dc4d32921e16155ef03f2d1147
karianmash <ianmachariak17@gmail.com>
2023-01-30 10:06:25 +0300

    Looked at let, var, const

--- scripts/index.js
+++ scripts/index.js
@@ -19,16 +19,18 @@ console.log(
 // console.log(x + y);
 // console.log(firstName);
 // console.log(typeof);
+// -----------------------------------------------------------------------------
 
 /*
  * Let keyword
  */
 let myAge = 41;
-// Can reassign variable 'myAge'
+// Can reassign variabltypeNumbere 'myAge'
 myAge = 42;
 // Cannot redeclare variable 'myAge'
 // let myAge = 43;
-console.log(myAge);
+// console.log(myAge);
+// -----------------------------------------------------------------------------
 
 /*
  * Var keyword
@@ -39,18 +41,59 @@ var myName = "John";
 // Can redeclare variable 'myName'
 var myName = "Jane";
 
-console.log(myName);
+// console.log(myName);
+// -----------------------------------------------------------------------------
 
 /*
  * Const keyword
  */
-const city = "Nairobi";
+// const city = "Nairobi";
 // Cannot reassign variable 'city'
 // city = "Mombasa";
 
 // Cannot redeclare variable 'city'
 // const city = "Mombasa";
 
-console.log(city);
+// console.log(city);
+// -----------------------------------------------------------------------------
 
-// Added a change
+/**
+ * Data types
+ */
+// Type Number
+let typeNumber = 5;
+// let typeNumber1 = 5.25;
+// Type String
+let typeString = "Michael";
+// let typeString1 = 'Hello';
+// let typeString2 = `Hello ${typeString}`;
+// Type Boolean
+let typeBoolean = true;
+// let typeBoolean1 = false;
+// Type Object
+let typeNull = null;
+// Type Undefined
+let typeUndefined = undefined;
+
+// console.log(typeString2);
+
+// console.log(typeof typeNumber);
+// console.log(typeof typeString);
+// console.log(typeof typeBoolean);
+// console.log(typeof typeNull);
+// console.log(typeof typeUndefined);
+
+// let undefinedTest;
+// let undefinedTest1 = undefined;
+// console.log(undefinedTest);
+
+// let nullTest1 = null;
+// console.log(nullTest1);
+// -----------------------------------------------------------------------------
+
+/**
+ * Operators
+ */
+
+// Modulus operator (%) 
+console.log(9 % 5);
