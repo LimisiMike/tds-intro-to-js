e2408f3f8bf9f5b589a115bcb4d9bbe58ea793ac
karianmash <ianmachariak17@gmail.com>
2023-01-31 09:07:36 +0300

    Covered conditional statements

---
+++ scripts/conditional.js
@@ -0,0 +1,31 @@
+// 1 If statements example
+let x = 5;
+
+if (x < 4) {
+  //   console.log(`${x} is less than 6`);
+}
+// -----------------------------------------------------------------------------
+
+// 2 If else statements example
+let y = 5;
+let z = 3;
+
+if (y < z) {
+  //   console.log(`${y} is less than ${z}`);
+} else {
+  //   console.log(`${y} is greater than ${z}`);
+}
+// -----------------------------------------------------------------------------
+
+// 3 If else if statements example
+let a = 5;
+let b = 5;
+
+if (a < b) {
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} is equal to ${b}`);
+}
+// -----------------------------------------------------------------------------
