21b6c6ea0fa1e65eadd8cc75e6c093f78dbba53b
karianmash <ianmachariak17@gmail.com>
2023-02-01 08:37:59 +0300

    Different ways of defining arrays

---
+++ scripts/4-arrays.js
@@ -0,0 +1,69 @@
+// Introduction to Arrays
+// -----------------------------------------------------------
+
+// Arrays are a way to store multiple values in a single variable.
+// Arrays are zero-indexed, meaning the first element is at index 0.
+// Arrays are also dynamic, meaning they can grow and shrink in size.
+// Arrays are also heterogenous, meaning they can store multiple types.
+// Arrays are created using square brackets.
+// Arrays are accessed using square brackets and an index.
+// Arrays are iterated using a for loop.
+
+// Create an array of 5 numbers
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// Create an array of 5 strings of user names
+let names = ["John", "Jane", "Jack", "Jill", "Joe"];
+
+// Create an array of 5 booleans
+let booleans = [true, false, true, false, true];
+
+// Create an array of 5 different data types
+let mixed = [1, "John", true, 3.14, null];
+
+// Create an array of 5 arrays
+let arrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [10, 11, 12],
+  [13, 14, 15],
+];
+
+// Create an array of 5 objects
+let objects = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Jack", age: 20 },
+  { name: "Jill", age: 15 },
+  { name: "Joe", age: 10 },
+];
+
+// Create an array of 5 functions
+let functions = [
+  function () {
+    return 1;
+  },
+  function () {
+    return 2;
+  },
+  function () {
+    return 3;
+  },
+  function () {
+    return 4;
+  },
+  function () {
+    return 5;
+  },
+];
+
+// Create an array of 5 arrays of 5 numbers
+let nested = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+];
+// -----------------------------------------------------------
