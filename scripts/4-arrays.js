fbf3ca57133b04077dea0b86c486bb70028646cb
karianmash <ianmachariak17@gmail.com>
2023-02-01 09:27:56 +0300

    Covered array methods

--- scripts/4-arrays.js
+++ scripts/4-arrays.js
@@ -1,4 +1,4 @@
-// Introduction to Arrays
+// (a) Introduction to Arrays
 // -----------------------------------------------------------
 
 // Arrays are a way to store multiple values in a single variable.
@@ -11,6 +11,7 @@
 
 // Create an array of 5 numbers
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// Index for array numbers   [0,  1,  2,  3,  4,  5,  6,  7]
 
 // Create an array of 5 strings of user names
 let names = ["John", "Jane", "Jack", "Jill", "Joe"];
@@ -67,3 +68,48 @@ let nested = [
   [21, 22, 23, 24, 25],
 ];
 // -----------------------------------------------------------
+
+// (b) Array methods
+// -----------------------------------------------------------
+// Access the first element of the numbers array
+// console.log(numbers[4]);
+
+// Replace the fifth element of the numbers array with 100
+// numbers[4] = 100;
+
+// Adding an element to the end of the numbers array
+numbers.push(100);
+
+// Adding an element to the beginning of the numbers array
+numbers.unshift(0);
+
+// Removing an element from the end of the numbers array
+numbers.pop();
+
+// Removing an element from the beginning of the numbers array
+numbers.shift();
+
+// console.log(numbers);
+
+// NOTE: Look at some more array methods:
+// -----------------------------------------------------------
+
+// (c) Iterating over arrays
+// -----------------------------------------------------------
+// Iterate over the numbers array and print each element to the console
+console.log("Looping using a regular loop!");
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+// Looping using a for...of loop
+console.log("Looping using a for... of loop!");
+for (let number of numbers) {
+  console.log(number);
+}
+
+// Looping using a forEach loop
+console.log("Looping using a forEach loop!");
+numbers.forEach(function (number) {
+  console.log(number);
+});
