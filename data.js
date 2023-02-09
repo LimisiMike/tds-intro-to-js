bbcc24c016401aa1421282a5b175bd5ad5e49dc9
karianmash <ianmachariak17@gmail.com>
2023-02-09 08:10:03 +0300

    Added some dummy data

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
 // Generate some random data objects to simulate an API response
 // This is just a helper function to generate some random data
 // for the demo. It's not part of the library.
-function generateData() {
+(function generateData() {
   var data = [];
-  for (var i = 0; i < 100; i++) {
+  for (var i = 1; i <= 100; i++) {
     data.push({
       id: i,
       name: "Item " + i,
       price: Math.floor(Math.random() * 1000),
     });
   }
+  console.log(data);
   return data;
-}
-// Path: data.js
+})();
