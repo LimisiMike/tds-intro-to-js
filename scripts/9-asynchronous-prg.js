1954aa48e3e5a46e4f9f1c6afc19d9ae6bc8b122
karianmash <ianmachariak17@gmail.com>
2023-02-09 09:27:50 +0300

    Looked at promises

--- scripts/9-asynchronous-prg.js
+++ scripts/9-asynchronous-prg.js
@@ -7,7 +7,7 @@
 // and is executed after some operation has completed.
 
 // Example 1
-// ----------------------------------------------------------------------
+// ----------------------------------------------------
 function greet(name) {
   console.log("Hello " + name);
 }
@@ -18,6 +18,52 @@ function processUserInput(nameArg, callback) {
   callback(name);
 }
 
-processUserInput("John", greet);
+// processUserInput("John", greet);
 
 // Example 2
+// ----------------------------------------------------
+function imDone() {
+  console.log("I'm done!");
+}
+
+function doSomething(callback) {
+  // setTimeout is a built-in function that takes a callback function as an
+  //   argument and executes it after a specified amount of time
+  setTimeout(function () {
+    console.log("I'm doing something!");
+
+    // Call the callback function
+    callback();
+  }, 3000);
+}
+
+// doSomething(imDone);
+// ----------------------------------------------------------------------
+
+// 2. Promises
+// ----------------------------------------------------------------------
+// A promise is an object that may produce a single value some time in the
+// future: either a resolved value, or a reason that it’s not resolved (e.g.,
+// a network error occurred). A promise may be in one of 3 possible states:
+// fulfilled, rejected, or pending. Promise users can attach callbacks to handle
+// the fulfilled value or the reason for rejection.
+
+// Example 1
+// ----------------------------------------------------
+// Create a promise
+let promise = new Promise(function (resolve, reject) {
+  // Do an async task async task and then...
+
+  if (1 > 2) {
+    // If it's successful
+    resolve();
+  } else {
+    // If it's failed
+    reject();
+  }
+});
+
+// Consume the promise
+promise
+  .then(() => console.log("Success!"))
+  .catch(() => console.log("Failure!"));
